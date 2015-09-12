3188c98f61a20fe08f4f6a54a2ebce4a9dd9baf6
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2015-09-12 12:47:46 +0000

    wasserman sim

--- inst/simulation_benchmarks/ddhw2_wasserman_normal_simulations_benchmark.R
+++ inst/simulation_benchmarks/ddhw2_wasserman_normal_simulations_benchmark.R
@@ -3,16 +3,16 @@
 # implement the below mainly for size-investing strategy illustration 
 library("ddhwPaper")
 
-register(MulticoreParam(workers=10, verbose=TRUE))
+register(MulticoreParam(workers=20, verbose=TRUE))
 
 #----------------- General benchmark settings -------------------------------#
 alphas <- .1#c(0.1, 0.01)
-nreps <- 2000 # number of times each simulation should be repeated (monte carlo replicates)
+nreps <- 500 # number of times each simulation should be repeated (monte carlo replicates)
 
 #------------- Simulation function ------------------------------------------#
 ms <- 20000
 
-xi_maxs<- seq(1.5, 6, length=20)
+xi_maxs<- seq(3, 6, length=10)
 
 sim_funs <- lapply(xi_maxs, function(x) wasserman_normal_sim_fun(20000,0.9,1, x))
 
@@ -20,16 +20,11 @@ sim_funs <- lapply(xi_maxs, function(x) wasserman_normal_sim_fun(20000,0.9,1, x)
 
 #------------- Methods to be benchmarked ------------------------------------#
 continuous_methods_list <- list(bh,
-							    storey_qvalue,
 							    lsl_gbh,
-							    tst_gbh,
-							    stratified_bh,
 							    clfdr,
 							    scott_fdrreg,
-							    ddhf,
-							    ddhw_5fold,
-							  	ddhw_5fold_reg,
-							    ddhw_unregularized)
+							  	ddhw_5fold_reg
+                  )
 
 fdr_methods <- lapply(continuous_methods_list, continuous_wrap)
 
@@ -39,4 +34,4 @@ eval_table <- run_evals(sim_funs, fdr_methods, nreps, alphas, BiocParallel=T, pr
 		xi_max = sapply(strsplit(eval_table$sim_pars,"xi_max:"),
 				function(x) as.numeric(x[2])))
 
-saveRDS(eval_table, file="result_files/ddhw2_wasserman_normal_simulation_benchmark.Rds")
+saveRDS(eval_table, file="result_files/ddhw2_wasserman_normal_simulation_benchmark_unbiased.Rds")
