a3016bb343c7ff0efd514c71b284847686cc4e6c
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2020-12-11 05:15:33 -0800

    temporarily stop testing the pasilla dataset

--- tests/testthat/test_analyze_datasets.R
+++ tests/testthat/test_analyze_datasets.R
@@ -1,6 +1,6 @@
 context("dataset_analysis")
 
 analyze_dataset("bottomly")
-analyze_dataset("pasilla")
+# analyze_dataset("pasilla"), until bug is fixed in pasilla pkg
 analyze_dataset("hammer")
 analyze_dataset("airway")
